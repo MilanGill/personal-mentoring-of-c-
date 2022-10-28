e5569fb62ddadf34ff38e072db4059a91410707b
Milan_Gill <milangill@yandex.ru>
2022-10-28 15:00:49 +0300

    начали создание своего массива, создали интерфейс

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -75,41 +75,7 @@ public:
     }
 };
 
-class Oleg
-{
-public:
-    Oleg()
-    {
-        Smart_Reference<int> i;
-        Smart_Reference<int> j;
-        cout << i << " , " << j << "\n";
-        i += 2;
-        cout << i << " , " << j << "\n";
-        j *= 5;
-        cout << i << " , " << j << "\n";
-        i = i + 2 * j;
-        cout << i << " , " << j << "\n";
-        cout << "-----------------------------------------------------------------------------\n";
-        char* p = nullptr;
-        /*cout << p;*/
 
-        // НАСТОЯЩИЕ РАНДОМНЫЕ ЧИСЛА 
-        /*std::random_device crypto_random_generator;
-        std::uniform_int_distribution<int> int_distribution(0, 9);
-
-        int actual_distribution[10] = { 0,0,0,0,0,0,0,0,0,0 };
-
-        for (int i = 0; i < 10000; i++) {
-            int result = int_distribution(crypto_random_generator);
-            actual_distribution[result]++;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            std::cout << actual_distribution[i] << " ";
-        }*/
-
-    }
-}oleg;
 ////template<>
 ////void array_mult(int* a, const int l, int k)
 //{
@@ -213,9 +179,37 @@ namespace Geometry
         Vector3D operator - () const
         {
             return Vector3D(-x, -y, -z);
-        }
 
-         Coord_Type operator [] (int k) const
+        }
+        
+        Coord_Type& operator [] (int k) 
+        {
+            if (k == 0)
+            {
+                return x;
+
+            }
+            else
+            {
+                if (k == 1)
+                {
+                    return y;
+
+                }
+                else
+                {
+                    if (k == 2)
+                    {
+                        return z;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+         const Coord_Type& operator [] (int k) const
         {
             
             if (k == 0) 
@@ -278,6 +272,10 @@ namespace Geometry
     typedef Vector3D <double> Vector;
 }
 
+
+
+
+
 class Klient
 {   
 public:
@@ -303,9 +301,35 @@ public:
 
 int Klient::quantity = 0;
 
+static int M = 0;
+
+void sqr_array2(int a[], int n) {
+    for (int i = 0; i < n; i+=4, a[i]/=M)
+    {
+        a[i] = a[i] + a[i];
+        a[i+1] = a[i+1] + a[i+1];
+        a[i + 2] = a[i + 2] + a[i + 2];
+        a[i + 3] = a[i + 3] + a[i + 3];
+    }
+}
+
+void sqr_array (int a[], int n) {
+    for (int i = 1; i < n; i++, a[i]/=M)
+    {
+        a[i] = a[i] + a[i];
+    }
+}
+const int l = 1000;
+static int a[l];
+class Init 
+{public:
+    Init() { for (int i = 0; i < l; i++) a[i] = 5; }
+    
+};
+
+
 int function(int argument) 
 {
-
     Klient& kl3 = *new Klient("Фантомас");
     Klient kl1("Милан");
     Klient kl2("Сергей");
@@ -320,14 +344,16 @@ int function(int argument)
     const int na = 1000 * 1000;
     int* a = new int[na];
     memset(a, 0 , sizeof(a[0]) * na);
-    __int64 t0 = clk();
     array_mult(a, na, 0);
+    __int64 t0 = clk();
+
+    sqr_array(a, l);
 
     __int64 t = clk();
     delete[] a;
     __int64 Dt = t - t0;
     cout << "Время = " << Dt <<" клоков = " <<double (Dt)/(1000*1000*1000) <<" секунд на 1 ГГЦ процессора" <<"\n" <<
-    double (Dt)/na << " клоков на сэмпл." << "\n";
+    double (Dt)/l << " клоков на сэмпл." << "\n";
     cout << "\n";
     cout << "\n";
 
@@ -412,7 +438,124 @@ int main()
 }
 
 
+template <typename T>
+class Array_Interface
+{public:
+    virtual ~Array_Interface() {}
+    virtual T& operator [] (int k) = 0;
+    virtual const T& operator [] (int k) const = 0;
+
+    virtual int size() = 0;
+    virtual void add(const T& element) = 0;
+    virtual void insert(int i, const T& element) = 0;
+    virtual void remove(int i, int n = 0) = 0;
+    virtual void add_array(const Array_Interface<T>& other_array) = 0;
+    virtual void insert_array(int i,const Array_Interface<T>& other_array) = 0;
+    virtual void on_error(const char * message) = 0;
+};
+
+template <typename T>
+class Array : public Array_Interface<T>
+{
+private :
+    T* a;
+    int n;
+    T& safe_element(int k) 
+    {
+#ifdef _DEBUG 
+        if (k < 0) on_error("\n k<0 !!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+        else if (k >= n) on_error("\n k >= n !!!!!!!!!!!!!!!!!!! \n");
+#else
+
+#endif
+        return a[k];
+    }
+public:
+    Array() : a(nullptr), n(0) {}
+    Array(int n, bool zero = true) : a(nullptr), n(0) {
+        if (n <= 0) {
+            on_error("You put size <= 0");
+            return;
+        }
+        a = ::new T[n];
+        this->n = n;
+        if (zero) 
+            for (int i = 0; i < n; i++) {
+                a[i] = T();
+            }
+        
+    }
+
+    virtual ~Array() {
+        if (a) {
+            delete[]a;
+            a = nullptr;
+        }
+    }
+    virtual T& operator [] (int k) {
+        return safe_element(k);
+    }
+    virtual const T& operator [] (int k) const {
+        return ((Array&)(*this)).safe_element(k);
+    }
+    template <typename T_>
+    Array& operator = (const Array_Interface<T_>& other_array) {
+        this -> ~Array();
+        
+        return *this;
+    }
+
+    
+    virtual int size() {
+        return n;
+    }
+protected:
+    virtual void add(const T& element) {}
+    virtual void insert(int i, const T& element) {}
+    virtual void remove(int i, int n = 0) {}
+    virtual void add_array(const Array_Interface<T>& other_array) {}
+    virtual void insert_array(int i, const Array_Interface<T>& other_array) {}
+    virtual void on_error(const char* message) {
+        cout << message << '\n';
+        std::runtime_error err(message);
+    }
+};
+#if 0
+template <typename T>
+class Array_ : public Array<T>
+{
+public:
+    virtual ~Array_Interface() {}
+    virtual T& operator [] (int k) {}
+    virtual const T& operator [] (int k) const {}
 
 
-// Попробовать анролинг на C 
-// 
+    virtual int size()
+    virtual void add(const T& element) {}
+    virtual void insert(int i, const T& element) {}
+    virtual void remove(int i, int n = 0) {}
+    virtual void add_array(const Array_Interface<T>& other_array) {}
+    virtual void insert_array(int i, const Array_Interface<T>& other_array) {}
+};
+#endif
+
+class Test_My_Array {
+public:
+    Test_My_Array() {
+        Array<int> a(100);
+        cout << a[99] << '\n';
+        const Array<int> b(10);
+        cout << b[20];
+    }
+}Test;
+
+
+
+// 1 - примеры |массив, матрица|
+// 2 - общее |интерфейсы, виртуальные функции и т.д + виртуальный диструктор|
+// 3 - Темплейты?
+// 4 - Windows Приложения 
+
+
+// Домашнее задание : 
+// Доделать Array и Array_ (+ проверить как все работает)
